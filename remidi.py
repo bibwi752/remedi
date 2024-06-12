7d755c58e122a1458003e01022ed50beecc1d38d
unknown <habibwicaksono752@gmail.com>
2024-06-12 09:59:25 +0700

    remedi

---
+++ remidi.py
@@ -0,0 +1,12 @@
+# apkdisain = ['CorelDRAW','adobe illustrator','adobe photoshop','adobe idesign','canva','inkscape','vectr','figma','sketch','affinity designer']
+# print ("apkdisain[7]: ", apkdisain[7])
+
+# english = {'big','best','busy','bitter','blind','bored','agree','alive','awful','afraid',}
+# print (english )
+
+def trapesium(a = 5, b = 7, t = 10 ):
+    sum = 0.5*(a+b)*t
+    print('sum: ',sum)
+
+    print('trpesium: ', trapesium)
+  
